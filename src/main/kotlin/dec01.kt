605b645773358a868b8f43ee55c26b2cc1d733cf
nickriordan <nick@anamatica.com>
2022-12-01 07:44:19 +0000

    Minor tidy up

--- src/main/kotlin/dec01.kt
+++ src/main/kotlin/dec01.kt
@@ -1,5 +1,5 @@
 fun dec01part1() = caloriesForEachElf().max()
-fun dec01part2() = caloriesForEachElf().sortedDescending().let { it[0] + it[1] + it[2] }
+fun dec01part2() = caloriesForEachElf().sorted().takeLast(3).sum()
 private fun caloriesForEachElf() =
     loadStrings("dec01.txt").fold(listOf(0)) { acc, i ->
         if (i.isEmpty()) acc + 0 else acc.dropLast(1) + (acc.last() + i.toInt())
