aa9f77665ef9ef247c08ff31f166181154673d9f
nickriordan <nick@anamatica.com>
2022-12-01 05:21:03 +0000

    December 1st!

--- src/main/kotlin/dec01.kt
+++ src/main/kotlin/dec01.kt
@@ -1,2 +1,6 @@
-fun dec01part1() = 0
-fun dec01part2() = 0
+fun dec01part1() = caloriesForEachElf().max()
+fun dec01part2() = caloriesForEachElf().sortedDescending().let { it[0] + it[1] + it[2] }
+private fun caloriesForEachElf() =
+    loadStrings("dec01.txt").fold(listOf(0)) { acc, i ->
+        if (i.isEmpty()) acc + 0 else acc.dropLast(1) + (acc.last() + i.toInt())
+    }
