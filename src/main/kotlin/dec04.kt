c89370646fddfd5faccfa80616e080a14c9fb439
nickriordan <nick@anamatica.com>
2022-12-03 10:26:27 +0000

    Prep for Dec 4th

---
+++ src/main/kotlin/dec04.kt
@@ -0,0 +1,13 @@
+fun dec04part1(): Int {
+
+    return 0
+}
+
+fun dec04part2(): Int {
+    return 0
+}
+
+private val input =
+    """
+        
+    """.trimIndent().split("\n")
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,6 @@
 fun main() {
     println("december 1st: ${dec01part1()}, ${dec01part2()}")
     println("december 2nd: ${dec02part1()}, ${dec02part2()}")
-    println("december 2nd: ${dec03part1()}, ${dec03part2()}")
+    println("december 3rd: ${dec03part1()}, ${dec03part2()}")
+    println("december 4th: ${dec04part1()}, ${dec04part2()}")
 }
