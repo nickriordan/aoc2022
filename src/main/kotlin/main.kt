28b482ee487da3f7966f8fba867b6d74f1b191bf
nickriordan <nick@anamatica.com>
2022-12-03 09:17:23 +0000

    December 3rd!

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,5 @@
 fun main() {
     println("december 1st: ${dec01part1()}, ${dec01part2()}")
     println("december 2nd: ${dec02part1()}, ${dec02part2()}")
+    println("december 2nd: ${dec03part1()}, ${dec03part2()}")
 }
