229b08a5cea37bf01f17cbca635348fdd028da4e
nickriordan <nick@anamatica.com>
2022-12-07 06:48:59 +0000

    December 7th

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,5 +4,6 @@ fun main() {
     println("3rd: ${dec03part1()}, ${dec03part2()}")
     println("4th: ${dec04part1()}, ${dec04part2()}")
     println("5th: ${dec05part1()}, ${dec05part2()}")
-    println("5th: ${dec06part1()}, ${dec06part2()}")
+    println("6th: ${dec06part1()}, ${dec06part2()}")
+    println("7th: ${dec07part1()}, ${dec07part2()}")
 }
